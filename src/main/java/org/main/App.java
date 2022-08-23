0a3726006582d5bbf76ee0e102dbb6adecb4114e
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-08-23 21:48:43 +0530

    police vs hackers multi thread game

--- src/main/java/org/main/App.java
+++ src/main/java/org/main/App.java
@@ -1,20 +1,26 @@
 package org.main;
 
+import org.thead.lesson1.HackerVsPoliceGame;
 import org.thead.lesson1.ThreadCreate;
+
+import java.util.Random;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        ThreadCreate threadCreate = new ThreadCreate();
+public class App {
+    public static void main(String[] args) {
+        /*ThreadCreate threadCreate = new ThreadCreate();
         try {
             threadCreate.createNewTread();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
+        }*/
+        hackerVsPoliceGameTrigger();
+    }
+
+    private static void hackerVsPoliceGameTrigger() {
+        HackerVsPoliceGame hackerVsPoliceGame = new HackerVsPoliceGame();
+        hackerVsPoliceGame.startGame();
     }
 }
