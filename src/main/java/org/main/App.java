60dee2138f1635899b7c8e85a60687d2875c884a
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-08-24 22:18:37 +0530

    thread.join example factorial calculation

--- src/main/java/org/main/App.java
+++ src/main/java/org/main/App.java
@@ -4,8 +4,12 @@ import org.thead.lesson1.HackerVsPoliceGame;
 import org.thead.lesson1.ThreadCreate;
 import org.thread.lesson2.BlockingTask;
 import org.thread.lesson2.InterruptSignalListener;
+import org.threadjoin.lesson3.FactorialThread;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -19,7 +23,13 @@ public class App {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }*/
-        hackerVsPoliceGameTrigger();
+        // hackerVsPoliceGameTrigger();
+
+        try {
+            calculateFactorialTest();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void hackerVsPoliceGameTrigger() {
@@ -42,5 +52,32 @@ public class App {
 
         // send interrupt signal
         thread.interrupt();
+    }
+
+    public static void calculateFactorialTest() throws InterruptedException {
+        List<Long> inputNumbers = Arrays.asList(0L, 3435L, 2324L, 4656L, 23L, 5556L);
+        List<FactorialThread> threads = new ArrayList<FactorialThread>();
+
+        for (long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
+
+        for (Thread t : threads) {
+            t.start();
+
+            // without below line application will terminate before done each thread work
+            // the 2000 means after 2 seconds thread will start next thread to process
+            t.join(2000);
+        }
+
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
+            }
+        }
+
     }
 }
